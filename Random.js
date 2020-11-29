a5a8d5b4bb11eebabcd9b3343447401bd5589811
Kyaara <7339.goyal@gmail.com>
2020-11-28 22:00:10 -0600

    Game one

---
+++ Random.js
@@ -0,0 +1,75 @@
+//** TUNNEL */
+function Tunnel(){
+    tunnel1=createSprite(1370,480,100,150);
+    tunnel1.addImage(tunnelImage2);
+    tunnel1.scale = 0.5;
+    //console.log(player.x);
+}
+
+
+//** TURTLES */
+function Turtles(){
+    var turtle;
+    turtle=createSprite();
+
+}
+
+
+//** COINS */
+function Coins(){
+    var coin;
+    coin=createSprite();
+
+}
+
+
+//** XP COINS */
+function XPcoins(){
+    var xpcoins;
+    xpcoins=createSprite();
+
+}
+function Brick(){
+    brick1=createSprite(970,360);
+    brick1.addImage(brickImage); 
+    brick1.scale=1.65;
+
+    brick2=createSprite(1064,360);
+    brick2.addImage(brickImage); 
+    brick2.scale=1.65;
+
+    brick3=createSprite(1160,360);
+    brick3.addImage(brickImage); 
+    brick3.scale=1.65;
+
+    brick4=createSprite(3665,360);
+    brick4.addImage(brickImage); 
+    brick4.scale=1.65;
+   
+    brick5=createSprite(3762,360);
+    brick5.addImage(brickImage); 
+    brick5.scale=1.65;
+ 
+    brick6=createSprite(3800,170);
+    brick6.addImage(brickImage); 
+    brick6.scale=1.65;
+
+    brick7=createSprite(3842,170);
+    brick7.addImage(brickImage); 
+    brick7.scale=1.65;
+
+    brick8=createSprite(3888,170);
+    brick8.addImage(brickImage); 
+    brick8.scale=1.65;
+
+    brick9=createSprite(3918,170);
+    brick9.addImage(brickImage); 
+    brick9.scale=1.65;
+
+    brick10=createSprite(3958,170);
+    brick10.addImage(brickImage); 
+    brick10.scale=1.65;
+    
+
+}
+
--- sketch.js
+++ sketch.js
@@ -1,9 +1,85 @@
+var player;
+var ground;
+var tunnel1, tunnel2, tunnel3;
+var groundImage;
+var brick1, brick2, brick3, brick4, brick5, brick6, brick7, brick8, brick9, brick10;
+var marioImage1, marioImage2, marioCollide, marioStand;
+var plantImage1, plantImage2, plantImage3, plantImage4;
+var turtleImage, turtleDying,turtleHiding;
+var brickImage, coinImage,CloudImage;
+var tunnelImage1, tunnelImage2;
+var fullBG;
+function preload(){
+  groundImage=loadImage("IMAGES/ground.png");
+
+  marioMove=loadAnimation("IMAGES/mario1.png", "IMAGES/mario2.png");
+  //marioImage2=loadImage();
+  marioCollide=loadImage("IMAGES/mario_collide.png");
+  marioStand=loadAnimation("IMAGES/mariostand.png");
+
+  plantImage1=loadImage("IMAGES/plant1.png");
+  plantImage2=loadImage("IMAGES/plant2.png");
+  plantImage=loadImage("IMAGES/plant3.png"); 
+  plantImage=loadImage("IMAGES/plant4.png");
+
+  turtleImage=loadImage("IMAGES/movingturtle.png");
+  turtleDying=loadImage("IMAGES/dyingtutle.jpeg");
+  turtleHiding=loadImage("IMAGES/hidingturtle.png");
+
+  tunnelImage1 = loadImage("IMAGES/tunnel1.png");
+  tunnelImage2 = loadImage("IMAGES/tunnel2.png");
+
+  brickImage=loadImage("IMAGES/brick.png");
+
+  coinImage=loadImage("IMAGES/coin.png");
+
+  CloudImage=loadImage("IMAGES/clouds.jpg");
+
+  fullBG = loadImage("IMAGES/fullBG.png");
+
+}
 function setup() {
-  createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+  createCanvas(1300,600);
+  player=createSprite(140, 505, 50, 50);
+  player.scale = 1.4;
+  player.addAnimation("stand",marioStand);
+  player.addAnimation("move", marioMove);
+  ground=createSprite(300,50);
+  ground.addImage(groundImage);
+  ground.scale=2;
+
+ 
+
+  
+  
+  //** ANIMATIONS */
 }
 
 function draw() {
-  background(255,255,255);  
+  background(0); 
+  image(fullBG, 0, 0, 10000, 600);
+  player.changeAnimation("stand");
+  if(keyDown(UP_ARROW)) {
+    player.velocityY=-2;
+  }
+  if(keyDown(DOWN_ARROW)) {
+    //CROUCHING ANIMATION
+  }
+  if(keyDown(LEFT_ARROW)) {
+    player.x=player.x-5;
+  }
+  if(keyDown(RIGHT_ARROW)) {
+    player.x=player.x+35;
+    player.changeAnimation("move");
+    camera.position.x = player.x+510;
+  } 
+
+  
+  //** CONTROLS OF PLAYER */
+   Tunnel();
+   Turtles();
+   Coins();
+   XPcoins();
+   Brick();
   drawSprites();
 }
